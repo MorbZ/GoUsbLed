c61ed21f4cf1e5edbed1e47670a87c6e77e5d7e0
Merten Peetz <dev@morbz.net>
2017-03-30 14:02:58 +0200

    Reconnect to Pusher after connection is lost

--- plugins/bitcoin.js
+++ plugins/bitcoin.js
@@ -1,35 +1,72 @@
 //requires
-var screen = require('../screen.js');
-var text = require('../text.js');
-var sprintf = require('sprintf-js').sprintf;
+const screen = require('../screen.js');
+const text = require('../text.js');
 
 //vars
-var isActive = false;
-var price;
+let isActive = false;
+let price = false;
+let activityTimer;
 
 //connect to websocket
-var WebSocket = require('ws')
-var ws = new WebSocket('ws://ws.pusherapp.com:80/app/de504dc5763aeef9ff52?client=js&version=2.2&protocol=5');
-ws.on('open', function() {
-	//subscribe to channel
-	var json = JSON.stringify({
-		'event': 'pusher:subscribe', 
-		'data': {
-			'channel': 'live_trades', 
-			'auth': null, 
+const WebSocket = require('ws')
+let ws;
+function connect() {
+	ws = new WebSocket('ws://ws.pusherapp.com:80/app/de504dc5763aeef9ff52?client=js&version=2.2&protocol=5');
+	ws.on('open', function() {
+		//subscribe to channel
+		pusherSend('pusher:subscribe', {
+			'channel': 'live_trades',
+			'auth': null,
 			'channel_data': {}
+		});
+	});
+	ws.on('message', function(message) {
+		resetActivityCheck();
+
+		let obj = JSON.parse(message);
+		let data = JSON.parse(obj.data);
+		if(data.price != undefined) {
+			price = Math.round(data.price).toString();
+			update();
 		}
 	});
-	ws.send(json);
-});
-ws.on('message', function(message) {
-	var obj = JSON.parse(message);
-	var data = JSON.parse(obj.data);
-	if(data.price != undefined) {
-		price = sprintf('%.1f', data.price);
+	ws.on('close', function() {
+	    // Reset price
+		price = false;
 		update();
+
+		// Reconnect
+		setTimeout(function() {
+			connect();
+		}, 5000);
+	});
+	ws.on('error', function(error) {
+	    console.log(error);
+	});
+}
+connect();
+
+function resetActivityCheck() {
+	if(activityTimer) {
+		clearTimeout(activityTimer);
 	}
-});
+
+	activityTimer = setTimeout(function() {
+		pusherSend('pusher:ping');
+
+		activityTimer = setTimeout(function() {
+			ws.terminate();
+		}, 10000);
+	}, 20000);
+}
+
+function pusherSend(event, data) {
+	let json = JSON.stringify({
+		'event': event,
+		'data': data
+	});
+	ws.send(json);
+}
 
 //update
 function update() {
@@ -37,11 +74,11 @@ function update() {
 	if(!isActive) {
 		return;
 	}
-	
+
 	//update screen
 	screen.clear();
-	var string;
-	if(price == undefined) {
+	let string;
+	if(price === false) {
 		string = '...';
 	} else {
 		string = price;
